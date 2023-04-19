78e965b8db5429958b7e6e50861e6d925eb81cc6
haseomg <lovethispool@kakao.com>
2023-04-20 04:53:18 +0900

    weather

---
+++ app/src/main/java/com/example/playlist/Weather.java
@@ -0,0 +1,12 @@
+package com.example.playlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Weather extends AppCompatActivity {
+
+    private static final String API_KEY = "5a620a6e8b9bbe71c32d3b1cf6f28455";
+
+    private static final String WEATHER_URL =
+            "https://api.openweathermap.org/data/2.5/weather?q={Suwon}&appid={5a620a6e8b9bbe71c32d3b1cf6f28455}";
+
+}
