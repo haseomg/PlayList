b21e3651a8490cc1733549d8b40a7e11a45f9b54
lovethispool <lovethispool@kakao.com>
2023-08-15 22:57:29 +0900

    Add songName, selectedTime TextView - for selected checking

--- app/src/main/java/com/example/playlist/Comment.java
+++ app/src/main/java/com/example/playlist/Comment.java
@@ -1,16 +1,26 @@
 package com.example.playlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Comment extends Activity {
 
-    Button back;
-    Button submit;
+    Button back, submit;
+    EditText writeComment;
+    TextView songName, selectedTime;
+    String getUserName, getSongName, getSelectedTime, msg;
+    ServerApi serverApi;
 
     // final = 변하지 않는 상수 - 다른 사람이 이 코드를 봐도 작성한 사람이 변하지 않게 설정한 것으로 알 수 있음
     public final String TAG = "[Comment Activity]";
@@ -23,6 +33,20 @@ public class Comment extends Activity {
         setContentView(R.layout.activity_comment);
         Log.i(TAG, "onCreate()");
 
+        textViewInitial();
+
+
+        serverApi = ApiClient.getApiClient().create(ServerApi.class);
+        Intent intent = getIntent();
+        getUserName = intent.getStringExtra("user_name");
+        Log.i(TAG, "getIntent getUserName : " + getUserName);
+        getSongName = intent.getStringExtra("song_name");
+        Log.i(TAG, "getIntent getSongName : " + getSongName);
+        getSelectedTime = intent.getStringExtra("selected_time");
+        Log.i(TAG, "getIntent getSelectedTime : " + getSelectedTime);
+
+        songName.setText(getSongName + " - ");
+        selectedTime.setText(getSelectedTime);
 
         back = findViewById(R.id.commentBackButton);
         back.setOnClickListener(new View.OnClickListener() {
@@ -32,14 +56,56 @@ public class Comment extends Activity {
             }
         });
 
+        writeComment = findViewById(R.id.commentEditText);
         submit = findViewById(R.id.commentSubmitButton);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // user_name, song_name, selected_time, msg to DB
+                msg = writeComment.getText().toString();
+                Log.i(TAG, "submit - getIntent getUserName : " + getUserName);
+                Log.i(TAG, "submit - getIntent getSongName : " + getSongName);
+                Log.i(TAG, "submit - getIntent getSelectedTime : " + getSelectedTime);
+                Log.i(TAG, "submit - writeComment check : " + msg);
+                // DB upload
+                comment_insert(getUserName, getSongName, getSelectedTime, msg);
+//                Intent commentIntent = new Intent();
+//                commentIntent.putExtra("user_name", getUserName);
+//                commentIntent.putExtra("song_name", getSongName);
+//                commentIntent.putExtra("selected_time", getSelectedTime);
+//                commentIntent.putExtra("msg", msg);
                 finish();
             }
         });
     }
+
+    void textViewInitial() {
+        songName = findViewById(R.id.commentSongNameTextView);
+        selectedTime = findViewById(R.id.commentSelectedTimeTextView);
+    } // textViewInitial END
+
+    public void comment_insert(String user, String song, String time, String msg) {
+        Log.i(TAG, "comment_insert");
+        Call<Void> call = serverApi.loadComment(user, song, time, msg);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.i(TAG, "comment_insert Method onResponse()");
+                if (response.isSuccessful()) {
+                    Log.i(TAG, "comment_insert Method onResponse() isSuccessful");
+                    Log.i(TAG, "Insert (response success)  and response.body check : " + response.body());
+                } else {
+                    Log.i(TAG, "comment_insert Method onResponse() !!!!!isSuccessful");
+                    Log.i(TAG, "Insert (response not successful)  and response.body check : " + response.body());
+                } // else
+            } // onResponse
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            } // onFailure
+        }); // call.enqueue
+    } // comment_insert
 
     protected void onStart() {
         super.onStart();
