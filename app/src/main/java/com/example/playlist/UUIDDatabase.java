a423fefe1ceef046a75e3f398ad47a2e224cc97b
lovethispool <lovethispool@kakao.com>
2023-10-09 21:56:31 +0900

    All Files Commit

--- app/src/main/java/com/example/playlist/UUIDDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.playlist;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Uuid.class}, version = 1, exportSchema = false)
-public abstract class UUIDDatabase extends RoomDatabase {
-    public abstract UuidDao uuidDao();
-}
