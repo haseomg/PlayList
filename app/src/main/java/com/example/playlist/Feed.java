b1d63eeeb0754dbbf6e704f55561f3e97a06ab8e
lovethispool <lovethispool@kakao.com>
2023-10-13 18:45:32 +0900

    [add] Feed - setCommentRecyclerView (item design not yet)

--- app/src/main/java/com/example/playlist/Feed.java
+++ app/src/main/java/com/example/playlist/Feed.java
@@ -8,11 +8,22 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
 
 public class Feed extends AppCompatActivity {
 
     TextView feedLogo, profileMusic;
     Button close;
+    ArrayList<FeedCommentModel> feedCommentList = new ArrayList<>();
+    androidx.recyclerview.widget.RecyclerView feedCommentRecyclerVIew;
+    FeedCommentModel feedCommentModel;
+    FeedCommentAdapter feedCommentAdapter;
+    LinearLayoutManager feedCommentLayoutManager;
+
+    private static final String BASE_URL = "http://54.180.155.66/";
+    private String song_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class Feed extends AppCompatActivity {
     } // onCreate
 
     private void initial() {
+
         feedLogo = findViewById(R.id.feedLogo);
         profileMusic = findViewById(R.id.feedProfileMusic);
         close = findViewById(R.id.feedCloseButton);
@@ -32,6 +44,13 @@ public class Feed extends AppCompatActivity {
         String user = intent.getStringExtra("user");
         feedLogo.setText(user);
         profileMusic.setText(user + " - hello ☁︎");
+
+        feedCommentRecyclerVIew = findViewById(R.id.feedCommentRecyclerView);
+         feedCommentLayoutManager = new LinearLayoutManager(this);
+         feedCommentRecyclerVIew.setLayoutManager(feedCommentLayoutManager);
+         feedCommentRecyclerVIew.setHasFixedSize(true);
+         feedCommentAdapter = new FeedCommentAdapter(this, feedCommentList);
+         feedCommentRecyclerVIew.setAdapter(feedCommentAdapter);
 
         close.setOnClickListener(new View.OnClickListener() {
             @Override
